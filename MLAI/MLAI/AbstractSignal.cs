fff8b6a0d39e7d33f26f44b5bfc5def4276788ef
jib fashr <fashrista@gmail.com>
2018-12-19 17:36:10 +0100

    ML framework project RFD detection automation
    Detects a sudden jump in signal/graph data/ time series

---
+++ MLAI/MLAI/AbstractSignal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Runtime.Api;
+
+
+
+
+namespace MLAI
+{
+    class AbstractSignal
+    {
+        [Column("0")]
+        public float Sig1;
+
+        [Column("1")]
+        public float Sig2;
+
+        [Column("2")]
+        public float Sig3;
+
+        [Column("3")]
+        public float Sig4;
+
+        [Column("4")]
+        public float Sig5;
+
+        [Column("5")]
+        public float Sig6;
+
+        [Column("6")]
+        public float Sig7;
+
+        [Column("7")]
+        public float Sig8;
+
+        [Column("8")]
+        public float Sig9;
+
+        [Column("9")]
+        public float Sig10;
+
+        [Column("10")]
+        public float Sig11;
+
+        [Column("11")]
+        public float Sig12;
+
+        [Column("12")]
+        public float Sig13;
+
+        [Column("13")]
+        public float Sig14;
+
+        [Column("14")]
+        public float Sig15;
+
+        [Column("15")]
+        public float Sig16;
+
+        [Column("16")]
+        public float Sig17;
+
+        [Column("17")]
+        public float Sig18;
+
+        [Column("18")]
+        public float Sig19;
+
+        [Column("19")]
+        public float Sig20;
+    }
+}
---
+++ MLAI/MLAI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.ML.Runtime.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mlai mlai = new Mlai();
+            Console.WriteLine(mlai.SayHello());
+            RegressionEvaluator.Result result = mlai.CreateAModel();
+            Console.WriteLine("RSquared:  " + result.RSquared + "  RMS: " + result.Rms);
+           Console.ReadKey();
+        }
+    }
+}
