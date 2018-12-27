85fc48e4613513bb4c4adc44f24c057a88c33689
jib fashr <fashrista@gmail.com>
2018-12-27 20:19:07 +0100

    Justsome small edits after testing
    zilch

--- MLAI/MLAI/Program.cs
+++ MLAI/MLAI/Program.cs
@@ -10,10 +10,11 @@ namespace MLAI
 {
     class Program
     {
-
+        //paths to training and testing data , and also path to trained model 
         static readonly string trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", @"C:\Users\Asus\AppData\LocalLow\S2P\Muscle board\TrainingSignals.csv");
         static readonly string testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", @"C:\Users\Asus\AppData\LocalLow\S2P\Muscle board\TrainingSignals1.csv");
         static readonly string modelPath = Path.Combine(Environment.CurrentDirectory, "Data", @"C:\Users\Asus\AppData\LocalLow\S2P\Muscle board\Model.zip");
+        static readonly string serverPath = Path.Combine(Environment.CurrentDirectory, "Data", @"C:\Users\Asus\AppData\LocalLow\S2P\Muscle board\server.csv");
         static void Main(string[] args)
         {
             Mlai mlai = new Mlai();
@@ -23,8 +24,7 @@ namespace MLAI
             var model = mlai.Train(mlContext, trainDataPath, textLoader, modelPath);
             var result = mlai.Evaluate(mlContext, model, testDataPath, textLoader);
             Console.WriteLine("RSquared:  " + result.RSquared + "  RMS: " + result.Rms);
-            Console.ReadKey();
-            List<double> signal = new List<double>() {1, 1,  1,  1 , 1 , 1,  1 , 1 , 1 , 1 ,1 ,1 ,1 ,1 ,15 ,1094843 ,484523,  1523 ,   6123 ,   9357};
+            List<double> signal = new List<double>() {31, 14,  133,  22 , 11 , 11,  16 , 18 , 12 ,12 ,14 ,11 ,13 ,14 ,15 ,143 ,484523,  123 ,   123 ,   957};
             float rfd = mlai.TestSinglePrediction(signal, mlContext, modelPath);
             Console.WriteLine("prediction:  " + rfd );
             Console.ReadKey();
